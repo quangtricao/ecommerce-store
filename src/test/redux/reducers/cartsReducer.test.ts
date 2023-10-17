399df8c637f71c4c74f902616b5dc7e70b3a118d
Tri Cao <tricq@flextech.vn>
2023-10-17 09:26:41 +0300

    Add test cases for cartsReducer

--- src/test/redux/reducers/cartsReducer.test.ts
+++ src/test/redux/reducers/cartsReducer.test.ts
@@ -4,6 +4,8 @@ import {
   removeFromCart,
   increaseQuantity,
   decreaseQuantity,
+  clearCart,
+  updateCart,
 } from "../../../redux/reducers/cartsReducer";
 import { mockProducts } from "../../mockProducts";
 
@@ -73,5 +75,43 @@ describe("cartReducer actions", () => {
     storeForTest.dispatch(decreaseQuantity(mockProducts[0].id));
 
     expect(storeForTest.getState().cartsReducer[0].number).toBe(3);
+  });
+
+  test("Clear the whole cart", () => {
+    storeForTest.dispatch(addToCart(mockProducts[0]));
+    storeForTest.dispatch(addToCart(mockProducts[1]));
+    storeForTest.dispatch(addToCart(mockProducts[2]));
+
+    storeForTest.dispatch(clearCart());
+    expect(storeForTest.getState().cartsReducer.length).toBe(0);
+  });
+
+  test("When ADMIN update a product, update the same product in cart", () => {
+    storeForTest.dispatch(addToCart(mockProducts[0]));
+    storeForTest.dispatch(addToCart(mockProducts[1]));
+    storeForTest.dispatch(addToCart(mockProducts[2]));
+
+    storeForTest.dispatch(
+      updateCart({
+        category: {
+          creationAt: "Time 2",
+          id: 5,
+          image: "Link for category 5",
+          name: "Category 5",
+          updatedAt: "Time 2",
+        },
+        creationAt: "Time 2",
+        description: "Product 2 update",
+        id: 2,
+        images: ["Link 1"],
+        price: 500,
+        title: "Product 2",
+        updatedAt: "Time 2",
+      })
+    );
+
+    expect(storeForTest.getState().cartsReducer[1].productInCart.description).toBe(
+      "Product 2 update"
+    );
   });
 });
